0162421c1dde80e25bc2e75cd5939903ecf872f1
Jerico <rickzx98@gmail.com>
2017-07-20 09:26:29 +0800

    updates

--- src/utils/convertToSubject.js
+++ src/utils/convertToSubject.js
@@ -6,8 +6,55 @@ export function convertToSubject(marcCode) {
     subject[Subject.THESAURUS] = marcCode.charAt(1);
     marcCode = marcCode.substr(2);
     let field = '';
+    let dollar = '';
+    let prevCode;
     for (let i = 0; i < marcCode.length; i++) {
-        
+        const code = marcCode.charAt(i);
+        if (code === '$') {
+            dollar = '$';
+        } else if (isMarcField(dollar + code)) {
+            if (prevCode && field.length > 0) {
+                if (subject[prevCode]) {
+                    let subar;
+                    if (subject[prevCode] instanceof Array) {
+                        subar = [...subject[prevCode], field];
+                    } else {
+                        subar = [subject[prevCode], field];
+                    }
+                    subject[prevCode] = subar;
+                } else {
+                    subject[prevCode] = field;
+                }
+
+            }
+            prevCode = dollar + code;
+            dollar = '';
+            field = '';
+        } else if (!isMarcField(dollar + code)) {
+            field += code;
+        }
+        if (i === marcCode.length - 1) {
+            subject[prevCode] = field;
+        }
     }
     return subject;
+}
+
+function isMarcField(chr) {
+    switch (chr) {
+        case Subject.TOPICAL_TERM_OR_GEOGRAPHIC_NAME_ENTRY:
+        case Subject.TOPICAL_TERM_FOLLOWING:
+        case Subject.LOCATION_OF_EVENT:
+        case Subject.ACTIVE_DATES:
+        case Subject.RELATOR_TERM:
+        case Subject.MISC_INFO:
+        case Subject.RELATIONSHIP:
+        case Subject.FORM_SUBDIVISION:
+        case Subject.GENERAL_SUBDIVISION:
+        case Subject.CHRONOLOGICAL_SUBDIVISION:
+        case Subject.GEOGRAPHIC_SUBDIVISION:
+            return true;
+        default:
+            return false;
+    }
 }
--- src/utils/index.spec.js
+++ src/utils/index.spec.js
@@ -9,6 +9,9 @@ describe('Util', () => {
             const subject = convertToSubject(marcSample);
             expect(subject[Subject.LEVEL_OF_SUBJECT]).toEqual('#');
             expect(subject[Subject.THESAURUS]).toEqual('0');
+            expect(subject[Subject.TOPICAL_TERM_OR_GEOGRAPHIC_NAME_ENTRY]).toEqual('Rain and rainfall');
+            expect(subject[Subject.GEOGRAPHIC_SUBDIVISION] instanceof Array).toEqual(true);
+            expect(subject[Subject.FORM_SUBDIVISION]).toEqual('Maps.');
         });
     });
 
