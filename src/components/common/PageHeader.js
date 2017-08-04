57a1f56ce4bcba9ed164e281601c82d92d9929bd
Jerico <rickzx98@gmail.com>
2017-08-04 16:35:38 +0800

    updates

--- src/components/common/PageHeader.js
+++ src/components/common/PageHeader.js
@@ -4,7 +4,7 @@ import React from 'react';
 export const PageHeader = ({ label, iconName }) => {
   return (<div className="page-header animated fadeIn">
     <div className="page-background-fader" />
-    <h3 className="page-header-title animated slideInDown">
+    <h3 className="page-header-title">
       <FontAwesome name={iconName} size="lg" fixedWidth={true} /><p> {label} </p></h3>
   </div>);
 };
