195a20e72a80effce06423a05e0e62abc1eac487
Jerico <rickzx98@gmail.com>
2017-08-09 17:30:08 +0800

    up

--- src/components/books/edit-page/BookSummary.js
+++ src/components/books/edit-page/BookSummary.js
@@ -1,25 +1,48 @@
+import { FormGroup, Img } from '../../common/';
+import {
+  LABEL_AUTHOR,
+  LABEL_BOOK_COVER,
+  LABEL_BOOK_INFORMATION,
+  LABEL_EDITION,
+  LABEL_ISBN_10,
+  LABEL_ISBN_13,
+  LABEL_NUMBER_OF_PAGES,
+  LABEL_PUBLISHED_DATE,
+  LABEL_PUBLISHER,
+  LABEL_SERIES_TITLE,
+  LABEL_SUB_TITLE,
+  LABEL_TITLE,
+  MESSAGE_AUTHOR_REQUIRED,
+  MESSAGE_ISBN_REQUIRED,
+  MESSAGE_PUBLISHED_DATE_REQUIRED,
+  MESSAGE_PUBLISHER_REQUIRED,
+  MESSAGE_TITLE_REQUIRED
+} from '../../../labels/';
+
 import { Book } from '../../../api/books/';
-import { Img } from '../../common/';
-import { LABEL_BOOK_INFORMATION } from '../../../labels/';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 export const BookSummary = ({ managedBook }) => {
-    return (<div>
-        <fieldset>
-            <legend>{LABEL_BOOK_INFORMATION}</legend>
-            <div className="col-sm-12">
-                <div className="col-sm-2 books">
-                    <div className="book">
-                        <Img src={managedBook[Book.IMAGE_URL]} />
-                    </div>
-                </div>
-                <div className="col-sm-10"></div>
-            </div>
-        </fieldset>
-    </div>);
+  return (<div className="books-summary">
+    <fieldset>
+      <legend>{LABEL_BOOK_INFORMATION}</legend>
+      <div className="col-sm-12">
+        <div className="col-sm-2 books">
+          <div className="book">
+            <Img src={managedBook[Book.IMAGE_URL]} />
+          </div>
+        </div>
+        <div className="col-sm-10">
+          <FormGroup label={LABEL_TITLE}>
+            <span>{managedBook[Book.TITLE]}</span>
+          </FormGroup>
+        </div>
+      </div>
+    </fieldset>
+  </div>);
 };
 
 BookSummary.propTypes = {
-    managedBook: PropTypes.object.isRequired
+  managedBook: PropTypes.object.isRequired
 }
