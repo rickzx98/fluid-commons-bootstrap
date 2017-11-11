913736020368963ad8278fc0c8d9df919f599a34
rickzx98 <rickzx98@gmail.com>
2017-11-11 11:39:31 +0800

    updates

--- src/actions/LoginActions.js
+++ src/actions/LoginActions.js
@@ -1,8 +1,28 @@
 import * as types from './';
 
-export function loginUser(credential) {
+export function requestLogin(credentials) {
     return {
-        type: types.LOGIN_USER,
-        credential
+        type: types.LOGIN_REQUEST,
+        isFetching: true,
+        isAuthenticated: false,
+        credentials
     };
+}
+
+export function receiveLogin(user) {
+    return {
+        type: types.LOGIN_SUCCESS,
+        isFetching: false,
+        isAuthenticated: false,
+        id_token: user.id.token
+    }
+}
+
+export function loginError(message) {
+    return {
+        type: types.LOGIN_FAILURE,
+        isFetching: false,
+        isAuthenticated: false,
+        message
+    }
 }
