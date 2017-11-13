bdc6820eb66bbdd50789315696321f74a23d9354
rickzx98 <rickzx98@gmail.com>
2017-11-13 19:53:07 +0800

    updates

---
+++ src/containers/ProfilePage.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { connect } from 'react-redux';
+
+export class ProfilePage extends React.Component {
+    render() {
+        return null;
+    }
+}
+
+ProfilePage.propTypes = {
+    security: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state) {
+    return {
+        security: state.security
+    };
+}
+
+export const ConnectProfilePage = connect(mapStateToProps)(ProfilePage);
